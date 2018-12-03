64cf43166d0acb9cc80212dfcbbd043e201dbc4b
Mary Wedd <weddm037@newschool.edu>
2018-12-03 18:51:54 -0500

    add image

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,10 @@
-questions.js
+// var txt = "contender";
+
+// document.write("<h4> LINK: " + txt.link("contenders.html") + "</h4>");
+
+document.getElementById("contenders").style.color = "red";
+
+function larger(){
+  document.getElementById("Question-1").style.width="600px";
+  document.getElementById("Question-1").style.height="300px";
+}
