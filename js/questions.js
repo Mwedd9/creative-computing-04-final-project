72944cef779dc926a81c66a73e35dc7a1e7fc0a3
Mary Wedd <weddm037@newschool.edu>
2018-12-03 20:28:30 -0500

    add

--- js/questions.js
+++ js/questions.js
@@ -2,7 +2,7 @@
 
 // document.write("<h4> LINK: " + txt.link("contenders.html") + "</h4>");
 
-document.getElementById("contenders").style.color = "red";
+// document.getElementById("contenders").style.color = "red";
 
 function larger(){
   document.getElementById("Box-3").style.width="600px";
